db8439525431020c849aecb665b5e0790352e133
dorkywaffles <vieraa@etsu.edu>
2022-09-01 18:06:58 -0400

    completed question 1

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Question1();
+
+static void Question1()
+{
+    Console.WriteLine("Enter a number: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Enter a number to add to {num1}: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"{num1} + {num2} = {(num1 + num2)}");
+}
