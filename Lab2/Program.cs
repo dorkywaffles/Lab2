ec4bec139b4a677f38818414af4dcde34f05408a
dorkywaffles <vieraa@etsu.edu>
2022-09-04 11:54:52 -0400

    completed question 3

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,5 +1,7 @@
-﻿Question1();
+﻿using ConsoleTables;
+Question1();
 Question2();
+Question3();
 
 // #1: Print the output of adding two numbers from user input
 static void Question1()
@@ -15,6 +17,7 @@ static void Question1()
 // The user can choose how high to multiply by.
 static void Question2()
 {
+    Console.WriteLine();
     Console.WriteLine("Enter a number: ");
     int num1 = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine($"Multiply {num1} by: ");
@@ -24,4 +27,26 @@ static void Question2()
     {
         Console.WriteLine($"{num1} * {i} = {(num1 * i)}");
     }
+}
+
+// #3: Output the number of bytes in memory that each of the following
+// number types uses and the minimum and maximum values they can have:
+// sbyte, byte, short, ushort, int, uint, long, ulong, float, double, and decimal.
+static void Question3()
+{
+    Console.WriteLine();
+    Console.WriteLine("Byte Table");
+    ConsoleTable table = new ConsoleTable("Type", "Byte(s) of memory", "Min", "Max");
+    table.AddRow("sbyte", sizeof(sbyte), sbyte.MinValue, sbyte.MaxValue);
+    table.AddRow("byte", sizeof(byte), byte.MinValue, byte.MaxValue);
+    table.AddRow("short", sizeof(short), short.MinValue, short.MaxValue);
+    table.AddRow("ushort", sizeof(ushort), ushort.MinValue, ushort.MaxValue);
+    table.AddRow("int", sizeof(int), int.MinValue, int.MaxValue);
+    table.AddRow("uint", sizeof(uint), uint.MinValue, uint.MaxValue);
+    table.AddRow("long", sizeof(long), long.MinValue, long.MaxValue);
+    table.AddRow("ulong", sizeof(ulong), ulong.MinValue, ulong.MaxValue);
+    table.AddRow("float", sizeof(float), float.MinValue, float.MaxValue);
+    table.AddRow("double", sizeof(double), double.MinValue, double.MaxValue);
+    table.AddRow("decimal", sizeof(decimal), decimal.MinValue, decimal.MaxValue);
+    Console.WriteLine(table);
 }
